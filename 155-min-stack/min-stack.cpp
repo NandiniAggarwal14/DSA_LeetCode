920ec82655c1592353e651bfec3b3086d62c8e76
Nandini Aggarwal <152698318+NandiniAggarwal14@users.noreply.github.com>
2025-09-01 17:34:56 +0530

    Time: 7 ms (20.06%) | Memory: 23.1 MB (93.87%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,27 +1,46 @@
 class MinStack {
 public:
-    stack<pair<int, int>> st;
+    stack<long long> st;
+    long long mini=LLONG_MAX;
     MinStack() {
     }
     
     void push(int val) {
         if(st.empty())
-            st.push({val, val});
+        {
+            mini=val;
+            st.push(val);
+        }
         else
-            st.push({val, min(val, st.top().second)});
+        {
+            if(val>mini)
+                st.push(val);
+            else
+                {
+                    st.push(2LL * val - mini);
+                    mini=val;
+                }
+        }
     }
     
     void pop() {
-        if(!st.empty())
-            st.pop();
+        if(st.empty())
+            return;
+        long long x = st.top();
+        st.pop();
+        if(x<mini)
+            mini= 2 * mini - x;
     }
     
     int top() {
-        return st.top().first;
+        long long x = st.top();
+        if(mini<x)
+            return (int)x;
+        return (int)mini;
     }
     
     int getMin() {
-        return st.top().second;
+        return (int)mini;
     }
 };
 
