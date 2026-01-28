6ee98f90e114a964927dbbd0b5a24c9140d150ef
Nandini Aggarwal <152698318+NandiniAggarwal14@users.noreply.github.com>
2026-01-28 17:15:40 +0530

    Time: 3 ms (33.09%) | Memory: 100.5 MB (32.66%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0 || n==1)
+            return true;
+        int index = 0;
+        for(int i=0; i<n-1; i++)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+                index = i;
+                break;
+            }
+        }
+        int diff = nums[index]-nums[index+1];
+        index+=1;
+        while(index<n-1)
+        {
+            if(diff< 0 && (nums[index]-nums[index+1]) >0)
+                return false;
+            else if(diff > 0 && (nums[index]-nums[index+1])<0)
+                return false;
+            else if(nums[index]==nums[index+1])
+            {
+                index++;
+                continue;
+                
+            }
+            else
+            {
+                index+=1;
+            }
+        }
+        return true;
+    }
+};
