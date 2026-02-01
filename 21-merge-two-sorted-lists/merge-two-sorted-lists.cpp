3d8824ac67eb2b73ce27ecdd40d210fd503e708f
Nandini Aggarwal <152698318+NandiniAggarwal14@users.noreply.github.com>
2026-02-01 20:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (61.54%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -13,27 +13,27 @@ public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode *head1=list1;
         ListNode *head2=list2;
-        ListNode *dummyNode= new ListNode(-1);
-        ListNode *temp=dummyNode;
-        while(head1!=NULL && head2!=NULL)
+        ListNode *dummy = new ListNode(-1);
+        ListNode *temp = dummy;
+        while(head1 != NULL && head2 != NULL)
         {
             if(head1->val < head2->val)
             {
-                temp->next=head1;
-                temp=head1;
-                head1=head1->next;
+                temp->next = head1;
+                temp = head1;
+                head1 = head1->next;
             }
             else
             {
-                temp->next=head2;
-                temp=head2;
-                head2=head2->next;
+                temp->next = head2;
+                temp = head2;
+                head2 = head2->next;
             }
         }
         if(head1)
-            temp->next=head1;
-        else 
-            temp->next=head2;
-        return dummyNode->next;
+            temp->next =  head1;
+        else
+            temp->next = head2;
+        return dummy->next;
     }
 };
