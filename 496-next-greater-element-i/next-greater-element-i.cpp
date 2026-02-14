757adb7b69d76e69e1340b5b17b6cd8c0c6f0ed0
Nandini Aggarwal <152698318+NandiniAggarwal14@users.noreply.github.com>
2026-02-14 10:45:02 +0530

    Time: 3 ms (42.29%) | Memory: 13 MB (13.92%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-        int n2=nums2.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
         vector<int>res(n1);
-        vector<int>nextGreater(n2);
+        vector<int>nge(n2);
         stack<int>st;
         for(int i=n2-1; i>=0; i--)
         {
-            while(!st.empty() && nums2[i] >= nums2[st.top()])
+            while(!st.empty() && nums2[i]>=nums2[st.top()])
                 st.pop();
             if(st.empty())
-                nextGreater[i]=-1;
+                nge[i] = -1;
             else
-                nextGreater[i]=nums2[st.top()];
+                nge[i] = nums2[st.top()];
             st.push(i);
         }
-        unordered_map<int, int> mpp;
+        unordered_map<int, int>mpp;
         for(int i=0; i<n2; i++)
-            mpp[nums2[i]] = nextGreater[i];
+        {
+            mpp[nums2[i]] = nge[i];
+        }
         for(int i=0; i<n1; i++)
+        {
             res[i] = mpp[nums1[i]];
+        }
         return res;
     }
 };
